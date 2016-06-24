b9ab0b3502f9bb8825a4c74962ae1d64d8173476
MacMini2 <djsucho@gmail.com>
2016-06-24 13:03:06 +0200

    EDIT FormContainers

--- forms/role/RoleFormContainer.php
+++ forms/role/RoleFormContainer.php
@@ -5,6 +5,7 @@ namespace Wame\PermissionModule\Forms;
 use Wame\DynamicObject\Forms\BaseFormContainer;
 use Wame\PermissionModule\Repositories\RoleRepository;
 
+
 interface IRoleFormContainerFactory
 {
 	/** @return RoleFormContainer */
@@ -17,7 +18,7 @@ class RoleFormContainer extends BaseFormContainer
 	/** @var array */
 	private $roleList;
 
-	
+
 	public function __construct(RoleRepository $roleRepository) 
 	{
 		parent::__construct();
@@ -25,28 +26,21 @@ class RoleFormContainer extends BaseFormContainer
 		$this->roleList = $roleRepository->getRoles();
 	}
 
-	
-    public function render() 
-	{
-        $this->template->_form = $this->getForm();
-        $this->template->render(__DIR__ . '/default.latte');
-    }
 
-	
     public function configure() 
 	{
 		$form = $this->getForm();
 
 		$form->addSelect('role', _('Role'), $this->roleList)
-				->setPrompt(_('- Select a role -'));
+				->setPrompt('- ' . _('Select a role') . ' -');
     }
-	
-	
+
+
 	public function setDefaultValues($object)
 	{
 		$form = $this->getForm();
 
 		$form['role']->setDefaultValue($object->userEntity->role);
 	}
-	
+
 }
