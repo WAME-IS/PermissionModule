b1cc161a5f4719f7d1bf4d84fc8730423bc271ba
Stanislav Červeňák <stano@wame.sk>
2016-09-23 12:27:18 +0200

    EDIT GRIDS add buttons

---
+++ vendor/wame/AdminModule/grids/toolbarButtons/Add.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Wame\PermissionModule\Vendor\Wame\AdminModule\Grids\ToolbarButtons;
+
+use Wame\AdminModule\Vendor\Wame\DataGridControl\ToolbarButtons\Add as AdminAdd;
+
+
+class Add extends AdminAdd
+{
+    public function __construct() 
+    {
+        $this->setTitle(_('Create role'));
+    }
+
+}
