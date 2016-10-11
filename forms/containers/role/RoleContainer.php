77338d4405464ad4beaddceac66cb57cc69fea9c
shadd0w <rene.gmitter@wame.sk>
2016-10-11 17:04:35 +0200

    UPDATE Permission

---
+++ forms/containers/role/RoleContainer.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Wame\PermissionModule\Forms\Containers;
+
+use Wame\DynamicObject\Forms\Containers\BaseContainer;
+use Wame\DynamicObject\Registers\Types\IBaseContainer;
+use Wame\PermissionModule\Repositories\RoleRepository;
+
+interface IRoleContainerFactory extends IBaseContainer
+{
+	/** @return RoleContainer */
+	public function create();
+}
+
+class RoleContainer extends BaseContainer
+{
+    /** @var array */
+	protected $roleList;
+    
+    
+    public function __construct(RoleRepository $roleRepository)
+    {
+        parent::__construct();
+        
+        $this->roleList = $roleRepository->getRoles();
+    }
+    
+    
+    /** {@inheritDoc} */
+    public function configure() 
+	{
+        $this->addSelect('role', _('Role'), $this->roleList);
+    }
+
+    /** {@inheritDoc} */
+	public function setDefaultValues($entity, $langEntity = null)
+	{
+        $this['role']->setDefaultValue($entity->getRole());
+	}
+    
+    /** {@inheritDoc} */
+    public function create($form, $values)
+    {
+        $form->getEntity()->setRole($values['role']);
+    }
+    
+    /** {@inheritDoc} */
+    public function update($form, $values)
+    {
+        $form->getEntity()->setRole($values['role']);
+    }
+
+}
