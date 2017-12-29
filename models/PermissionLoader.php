24a80664f3958d3dda39f3efc7c1269f4b7f022f
Stanislav Červeňák <stano@wame.sk>
2017-12-29 11:18:04 +0100

    FIX initialize without database

--- models/PermissionLoader.php
+++ models/PermissionLoader.php
@@ -8,31 +8,34 @@ use Wame\PermissionModule\Repositories\PermissionRepository;
 use Wame\PermissionModule\Models\PermissionObject;
 use Wame\PermissionModule\Entities\PermissionEntity;
 
-class PermissionLoader extends Object 
+
+class PermissionLoader extends Object
 {
-	/** @var EntityManager */
+    /** @var EntityManager */
     private $entityManager;
-    
-    
+
+
     public function __construct(EntityManager $entityManager)
     {
         $this->entityManager = $entityManager;
     }
-    
 
-	public function setup(PermissionObject $permissionObject) 
-	{
-		$permissions = $this->entityManager->getRepository(PermissionEntity::class)->findBy([]);
-        
-		foreach ($permissions as $permission) {
-			if ($permission->tag == PermissionRepository::TAG_ALLOW) {
-				$permissionObject->allow($permission->role->getName(), $permission->resource, $permission->action);
-			} elseif ($permission->tag == PermissionRepository::TAG_OWN) {
-				$permissionObject->allow($permission->role->getName(), $permission->resource, $permission->action . PermissionObject::ACESS_OWN_CHAR);
-			} elseif ($permission->tag == PermissionRepository::TAG_DENY) {
-				$permissionObject->deny($permission->role->getName(), $permission->resource, $permission->action);
-			}
-		}
-	}
+
+    public function setup(PermissionObject $permissionObject)
+    {
+        try {
+            $permissions = $this->entityManager->getRepository(PermissionEntity::class)->findBy([]);
+
+            foreach ($permissions as $permission) {
+                if ($permission->tag == PermissionRepository::TAG_ALLOW) {
+                    $permissionObject->allow($permission->role->getName(), $permission->resource, $permission->action);
+                } elseif ($permission->tag == PermissionRepository::TAG_OWN) {
+                    $permissionObject->allow($permission->role->getName(), $permission->resource, $permission->action . PermissionObject::ACESS_OWN_CHAR);
+                } elseif ($permission->tag == PermissionRepository::TAG_DENY) {
+                    $permissionObject->deny($permission->role->getName(), $permission->resource, $permission->action);
+                }
+            }
+        } catch (\Exception $e) {}
+    }
 
 }
--- models/RoleLoader.php
+++ models/RoleLoader.php
@@ -6,35 +6,45 @@ use Kdyby\Doctrine\EntityManager;
 use Wame\PermissionModule\Models\PermissionObject;
 use Wame\PermissionModule\Entities\RoleEntity;
 
+
 class RoleLoader
 {
     use \Nette\SmartObject;
-    
-    
+
+
     /** @var EntityManager */
     private $entityManager;
-    
-    
+
+
     public function __construct(EntityManager $entityManager)
     {
         $this->entityManager = $entityManager;
     }
-    
-    
+
+
     /**
      * Setup roles
-     * 
+     *
      * @param PermissionObject $permission
      */
     public function setup(PermissionObject $permission)
     {
-        $roles = $this->entityManager->getRepository(RoleEntity::class)->findBy([]);
-        
-        foreach($roles as $role) {
-            $roleName = $role->getName();
-            $inheritBy = $role->getInherit() ? [$role->getInherit()->getName()] : null;
-            $permission->addRole($roleName, $inheritBy);
+        try {
+            $roles = $this->entityManager->getRepository(RoleEntity::class)->findBy([]);
+
+            foreach ($roles as $role) {
+                $roleName = $role->getName();
+                $inheritBy = $role->getInherit() ? [$role->getInherit()->getName()] : null;
+                $permission->addRole($roleName, $inheritBy);
+            }
+        } catch (\Exception $e) {
+            $permission->addRole('guest');
+            $permission->addRole('client', ['guest']);
+            $permission->addRole('moderator', ['client']);
+            $permission->addRole('admin', ['guest']);
+
+//            throw new \Exception($e);
         }
     }
-    
+
 }
